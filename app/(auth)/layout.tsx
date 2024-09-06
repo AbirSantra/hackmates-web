b22e38dcbcb2eed1437b2400ef0594128744914c
Abir Santra <santraabir8@gmail.com>
2024-09-06 12:30:29 +0530

    minor padding fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,17 +6,16 @@ export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
-  }) {
-
+}) {
   return (
     <div className="relative flex min-h-dvh w-full flex-col-reverse bg-zinc-100 lg:flex-row">
       <section className="flex h-full flex-col rounded-t-[32px] bg-white p-6 shadow-2xl md:flex lg:h-dvh lg:w-1/2 lg:rounded-r-[32px] lg:rounded-tl-none lg:shadow-sm">
         <AuthHeader view="large" />
-        <div className="flex w-full flex-1 items-center justify-center">
+        <div className="flex w-full flex-1 items-center justify-center py-4">
           {children}
         </div>
       </section>
-      <section className="flex w-full flex-1 select-none flex-col items-center gap-4 p-6 py-4 lg:justify-center">
+      <section className="flex w-full flex-1 select-none flex-col items-center gap-4 p-6 lg:justify-center">
         <AuthHeader view="small" />
         <div className="my-auto flex w-full max-w-sm lg:my-0 lg:max-w-lg">
           <Image
